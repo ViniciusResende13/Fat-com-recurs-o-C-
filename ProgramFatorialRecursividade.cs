6292cb38ec79d0c8ae72baa9374b158caff6125f
Vinicius Resende do Carmo <105870043+ViniciusResende13@users.noreply.github.com>
2022-09-18 15:48:53 -0300

    Add files via upload

---
+++ ProgramFatorialRecursividade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial_com_função_recursiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Digite o número que você quer o fatorial: ");
+            n= int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\nO fatorial é: ");
+            Console.WriteLine(fat(n));
+
+
+            Console.ReadKey();
+        }
+        static int fat (int n)
+        {
+            int res;
+            if (n <= 1)
+            {
+                res = 1;
+            }
+            else
+            {
+                res = n*fat(n-1);
+            }
+            return res;
+        }
+    }
+}
